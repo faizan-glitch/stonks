941cce201d722bfb3a7fb7e7c57865c180f47175
Faizan <faizan-666@outlook.com>
2022-04-22 20:25:06 +0500

    Added Update and generateRandomSymbol in stocks.go

--- pkg/stocks/stocks.go
+++ pkg/stocks/stocks.go
@@ -1,6 +1,9 @@
 package stocks
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 type Stock struct {
 	Time   time.Time `json:"time"`
@@ -13,5 +16,32 @@ type Stock struct {
 }
 
 func (s *Stock) Update() {
-	// TODO: Update the stock
+	rand.Seed(time.Now().Unix())
+
+	s.Time = time.Now()
+
+	s.Symbol = generateRandomSymbol()
+
+	// Todo: Calculate s.Close according to the requirements
+
+	if s.Close > s.High {
+		s.High = s.Close
+	} else {
+		s.Low = s.Close
+	}
+	s.Volume = rand.Intn(1000)
+}
+
+func generateRandomSymbol() string {
+	sym := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+	output := make([]byte, 4)
+
+	// generate a random symbol of length 4
+
+	for i := 0; i < 4; i++ {
+		output[i] = sym[rand.Intn(26)]
+	}
+
+	return string(output)
 }
